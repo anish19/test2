ba39f5c1f90a977b24a1db81fc781c7d0de242b7
anish19 <hoopanish@gmail.com>
2014-07-27 17:13:00 +0530

    Message display app

--- src/com/example/test2/MainActivity.java
+++ src/com/example/test2/MainActivity.java
@@ -1,17 +1,19 @@
 package com.example.test2;
 
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
+
+    public final static String EXTRA_MESSAGE = "com.example.test2.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +26,14 @@ public class MainActivity extends Activity {
                     .commit();
         }
     }
-
+    
+    public void sendMessage(View view){
+    	Intent intent = new Intent(this, DisplayMessageActivity.class);
+	    EditText editText = (EditText) findViewById(R.id.edit_message);
+	    String message = editText.getText().toString();
+	    intent.putExtra(EXTRA_MESSAGE, message);
+	    startActivity(intent);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
